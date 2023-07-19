5b751708e56a79ed5d7c808b336b86767e8e2d6f
virajchhayani <virajchhayani9999@gmail.com>
2023-07-19 19:14:21 +0530

    .

--- First/ex12.cs
+++ First/ex12.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace First
 {
-    internal class ex12
+    internal class Employee
     {
+        int EmpID;
+        string Name;
+        string City;
+        public Employee()
+        {
+
+        }
+        public Employee(int EmpID, string Name)
+        {
+            this.EmpID = EmpID;
+            this.Name = Name;
+        }
+        public Employee(int EmpID, string Name, string City) : this(EmpID, Name)
+        {
+            this.City = City;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"EmpID: {EmpID}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"City: {City}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee obj1 = new Employee();
+            Employee obj2 = new Employee(101, "Viraj", "Jasdan");
+
+            obj1.Display();
+            Console.WriteLine();
+            obj2.Display();
+
+            Console.ReadKey();
+        }
+        
     }
 }
